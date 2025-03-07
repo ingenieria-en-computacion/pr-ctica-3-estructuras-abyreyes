e8c4890ccf4882d13885c2be3a7dadd698a898ac
abyreyes <aby.08reyes@gmail.com>
2025-03-06 20:59:17 -0600

    Create conjunto.h

---
+++ conjunto.h
@@ -0,0 +1,22 @@
+#ifndef __CONJUNTO_H__
+#define __CONJUNTO_H__
+#define TAM 1000
+#include <stdbool.h>
+
+typedef int DATO;
+
+typedef struct Conjunto{
+    DATO datos[TAM];
+    short cant;
+} CONJUNTO;
+
+CONJUNTO conjunto_vacio();
+CONJUNTO agregar(CONJUNTO, DATO);
+bool pertenece(CONJUNTO, DATO);
+CONJUNTO quitar(CONJUNTO, DATO);
+bool es_vacio(CONJUNTO);
+int cardinal(CONJUNTO);
+CONJUNTO _union(CONJUNTO, CONJUNTO);
+void print_conjunto(CONJUNTO);
+
+#endif
